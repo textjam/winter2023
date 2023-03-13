3614723cc98715de956b89c75d75f176f69ef23d
Jack Arthur Harrhy <me@jackharrhy.com>
2023-03-13 19:41:39 -0230

    collections for submission & updates, many changes
    - SubmissionList component
    - submissions and update schemas in the new config.ts file
    - blascii & urists quest examples from amelie and ethan
    - unnamed wip entry for me :)
    - update to indicate start of jam
    - wip how-to-submit page since i want to get this commit in first
      before writing this section
    - css changes, border-box & p tags less margin
    - page layout component since we have more than one page now
    - first usage of the Intl API for myself!

---
+++ src/content/config.ts
@@ -0,0 +1,27 @@
+import { z, defineCollection } from "astro:content";
+
+const submissionsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    creator: z.string(),
+    creatorLink: z.string().optional(),
+    using: z.array(z.string()).optional(),
+    githubProfile: z.string().optional(),
+    githubLink: z.string().optional(),
+    itchioLink: z.string().optional(),
+    wip: z.boolean().default(false),
+    example: z.boolean().default(false),
+  }),
+});
+
+const updatesCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+  }),
+});
+
+export const collections = {
+  submissions: submissionsCollection,
+  updates: updatesCollection,
+};
---
+++ src/content/submissions/example-blascii.md
@@ -0,0 +1,21 @@
+---
+title: BLASCII
+creator: Ethan Denny
+creatorLink: https://github.com/EthanDenny/
+using:
+  - Python
+githubProfile: https://github.com/EthanDenny/
+githubLink: https://github.com/EthanDenny/blascii
+itchioLink: https://asaltynewfie.itch.io/blascii
+example: True
+---
+<pre>A text-based shooter sorta-kinda-like Nuclear Throne.
+
+Run it with init.py.
+
+Controls:
+
+WASD - Move
+IJKL - AIM
+Space - Shoot
+</pre>
